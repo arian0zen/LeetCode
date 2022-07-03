b5e9a1db35217bc828aaa7a9742356fdeb5b9e61
Arian Shaikh <ariansk90@gmail.com>
2022-07-03 22:44:54 +0530

    Update minimum_number_of_moves_to_seat_everyone.py

--- minimum_number_of_moves_to_seat_everyone.py
+++ minimum_number_of_moves_to_seat_everyone.py
@@ -4,10 +4,10 @@ def minMovesToSeat(seats, students):
     students.sort()
     if len(seats) == len(students):
         moves = 0
-    for i, j in zip(seats, students):
-        if i!= j:
-            moves += abs(i-j)
-    return moves
+        for i, j in zip(seats, students):
+            if i!= j:
+                moves += abs(i-j)
+        return moves
 
 seats = [3,1,5]
 students = [2,7,4]
