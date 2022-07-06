7f81c03949cd3bb8b211c909ba9ea18b8e82ff19
Arian Shaikh <ariansk90@gmail.com>
2022-07-06 20:13:39 +0530

    6th july

---
+++ check_if_a_word_occurs_as_a_prefix.py
@@ -0,0 +1,17 @@
+def isPrefixOfWord(sentence, searchWord):
+    temp_a = sentence.split(' ')
+    print(temp_a)
+    for i in range(len(temp_a)):
+    	if temp_a[i].startswith(searchWord):
+            return i + 1
+
+    return -1
+
+        
+    
+	
+ 
+sentence = "leetcode corona"
+searchWord = "leet"
+print(isPrefixOfWord(sentence, searchWord))
+        
