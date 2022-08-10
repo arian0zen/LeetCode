04b64a3c7e28a4e0b9396675cd0f91b7b9e4f42b
Arian Shaikh <ariansk90@gmail.com>
2022-08-10 19:27:55 +0530

    Create convert_sortedArrayTo_BST.py

---
+++ convert_sortedArrayTo_BST.py
@@ -0,0 +1,16 @@
+def convertToBST(nums):
+    def helper(l, r, array):
+        if l > r:
+            return None
+        mid = (l + r) // 2
+        array.append(nums[mid])
+        helper(l, mid-1, array)
+        helper(mid+1, r, array)
+        return array
+    return helper(0, len(nums) - 1, [])
+
+
+nums = [-10,-3,0,5,9]
+print(convertToBST(nums))
+
+#return traversal of the BST
