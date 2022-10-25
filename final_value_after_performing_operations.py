477bae5887a4eef57b3446b2337bf441622109ae
Arian Shaikh <68517592+arian0zen@users.noreply.github.com>
2022-10-25 15:53:43 +0530

    Update final_value_after_performing_operations.py

--- final_value_after_performing_operations.py
+++ final_value_after_performing_operations.py
@@ -4,7 +4,7 @@ from typing import final
 def finalVal(operations):
     count = 0
     for i in operations:
-        if i[1] == "+": #the array elements are strings, and the 0 th and 2nd changes but the 1th dont, 1thy always representing the operations. 
+        if i[1] == "+": #the array elements are strings, and the 0th and 2nd changes but the 1th dont, 1thy always representing the operations. 
             #++X ---> + > 0 ; + > 1 ; X > 2 || X++ ---> X > 0 ; + > 1 ; + > 2
             count += 1
         else:
